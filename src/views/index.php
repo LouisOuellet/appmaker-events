680d2958dabd8261683e5231ec573bbeae908802
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-30 12:27:41 -0500

    21.11-dev-7

--- src/views/index.php
+++ src/views/index.php
@@ -1,2 +1,1 @@
-<link rel="preload" href="plugins/events/dist/fonts/Scriptina.woff2" as="font" type="font/woff2" crossorigin>
-<link rel="preload" href="plugins/events/dist/fonts/SignPainterHouseScript.woff2" as="font" type="font/woff2" crossorigin>
+<div id="pagecontent"></div>
