679e33e90cf50f417cae8851b31bb7db4d497acb
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-01 14:02:31 -0500

    21.12-dev-78

--- dist/js/script.js
+++ dist/js/script.js
@@ -657,7 +657,7 @@ API.Plugins.events = {
 										}
 										if(API.Helper.isSet(hosts,[API.Contents.Auth.User.id])){
 											html += '<div class="col-12">';
-												html += '<button class="btn btn-warning btn-lg btn-block" data-action="Upload"><i class="fas fa-plus"></i></button>';
+												html += '<button class="btn btn-warning btn-lg btn-block" data-action="Download"><i class="fas fa-angle-down mr-1"></i>'+API.Contents.Language["Download All"]+'</button>';
 											html += '</div>';
 										}
 									html += '</div>';
